35c37128856571e6e978e573253f3896544d6973
DeltaGynbot <95764801+DeltaGynbot@users.noreply.github.com>
2022-02-08 01:19:17 -0300

    código do robô no arduino0

--- main.ino
+++ main.ino
@@ -11,158 +11,157 @@ static int signal(int number) {
 	return number > 0 ? 1 : -1;
 }
 
-class Ultrasonic {
-	public:
-		int trigger;
-		int echo;
+struct Ultrasonic {
+	int trigger;
+	int echo;
 
-		Ultrasonic(int _trigger, int _echo) {
-			trigger = _trigger;
-			echo = _echo;
+	Ultrasonic(int _trigger, int _echo) {
+		trigger = _trigger;
+		echo = _echo;
 
-			pinMode(trigger, OUTPUT);
-			pinMode(echo, INPUT);
+		pinMode(trigger, OUTPUT);
+		pinMode(echo, INPUT);
 
-			digitalWrite(trigger, LOW);
-		}
+		digitalWrite(trigger, LOW);
+	}
 
-		void disable() {
-			digitalWrite(trigger, LOW);
-		}
+	void disable() {
+		digitalWrite(trigger, LOW);
+	}
+	
+	int read() {
+		digitalWrite(trigger, HIGH); // the trigger pin send a signal
+		delayMicroseconds(10);       // to 10us
+		digitalWrite(trigger, LOW);
 		
-		int read() {
-			digitalWrite(trigger, HIGH); // the trigger pin send a signal
-			delayMicroseconds(10);       // to 10us
-			digitalWrite(trigger, LOW);
-			
-			return 17 * (pulseIn(echo, HIGH) / 100.0);
-		}
+		return 17 * (pulseIn(echo, HIGH) / 100.0);
+	}
 };
 
-class Inclination {
-	public:
-		int output;
+struct Inclination {
+	int output;
 
-		Inclination(int _output) {
-			output = _output;
+	Inclination(int _output) {
+		output = _output;
 
-			pinMode(output, INPUT);
-		}
+		pinMode(output, INPUT);
+	}
 
-		bool read() {
-			return digitalRead(output);
-		}
+	bool read() {
+		return digitalRead(output);
+	}
 };
 
-class Obstacle {
-	public:
-		int output;
+struct Obstacle {
+	int output;
 
-		Obstacle(int _output) {
-			output = _output;
+	Obstacle(int _output) {
+		output = _output;
 
-			pinMode(output, INPUT);
-		}
+		pinMode(output, INPUT);
+	}
 
-		bool read() {
-			return digitalRead(output);
-		}
+	bool read() {
+		return digitalRead(output);
+	}
 };
 
-class Infrared {
-	public:
-		int output;
+struct Infrared {
+	int output;
+	int threshold;
 
-		Infrared(int _output) {
-			output = _output;
+	Infrared(int _output) {
+		output = _output;
 
-			pinMode(output, INPUT);
-		}
+		threshold = 90;
 
-		int read() {
-			return map(analogRead(output), 0, 1023, 100, 0);
-		}
+		pinMode(output, INPUT);
+	}
+
+	int read() {
+		int value = map(analogRead(output), 0, 1023, 100, 0);
+
+		return value > threshold ? 0 : 1;
+	}
 };
 
-class Servo {
-	public:
-		int control;
-		int position = false; 
+struct Servo {
+	int control;
+	int position = false; 
 
-		Servo(int _control) {
-			control = _control;
+	Servo(int _control) {
+		control = _control;
 
-			pinMode(control, OUTPUT);
+		pinMode(control, OUTPUT);
 
+		digitalWrite(control, LOW);
+	}
+
+	void disable() {
+		digitalWrite(control, LOW);
+	}
+
+	void write(int angle) {
+		limit(angle, 0, 180);
+
+		int time = map(angle, 0, 180, 500, 2500);
+
+		for (int iter = 1; iter <= 100; iter++) {
+			digitalWrite(control, HIGH);
+			delayMicroseconds(time);
 			digitalWrite(control, LOW);
+			delayMicroseconds(20000 - time);
 		}
 
-		void disable() {
-			digitalWrite(control, LOW);
-		}
+		position = angle;
+	}
 
-		void write(int angle) {
-			limit(angle, 0, 180);
-
-			int time = map(angle, 0, 180, 500, 2500);
-
-			for (int iter = 1; iter <= 100; iter++) {
-				digitalWrite(control, HIGH);
-				delayMicroseconds(time);
-				digitalWrite(control, LOW);
-				delayMicroseconds(20000 - time);
-			}
-
-			position = angle;
-		}
-
-		int read() {
-			return position;
-		}
+	int read() {
+		return position;
+	}
 };
 
-class Motor {
-	public:
-		int controll;
-		int controlr;
+struct Motor {
+	int controll;
+	int controlr;
 
-		int velocity;
+	int velocity;
 
-		Motor(int _controll, int _controlr) {
-			controll = _controll;
-			controlr = _controlr;
-			
-			pinMode(controll, OUTPUT); digitalWrite(controll, LOW);
-			pinMode(controlr, OUTPUT); digitalWrite(controlr, LOW);
-		}
+	Motor(int _controll, int _controlr) {
+		controll = _controll;
+		controlr = _controlr;
 		
-		void disable() {
-			digitalWrite(controll, LOW);
-			digitalWrite(controlr, LOW);
+		pinMode(controll, OUTPUT); digitalWrite(controll, LOW);
+		pinMode(controlr, OUTPUT); digitalWrite(controlr, LOW);
+	}
+	
+	void disable() {
+		digitalWrite(controll, LOW);
+		digitalWrite(controlr, LOW);
+	}
+	
+	void write(int _velocity) {
+		velocity = _velocity;
+
+		limit(velocity, -100, 100);
+
+		short int inputl = 0;
+		short int inputr = 0;
+
+		if (velocity > 0) {
+			inputl = map(abs(velocity), 0, 100, 0, 255);
 		}
-		
-		void write(int _velocity) {
-			velocity = _velocity;
-
-			limit(velocity, -100, 100);
-
-			short int inputl = 0;
-			short int inputr = 0;
-
-			if (velocity > 0) {
-				inputl = map(abs(velocity), 0, 100, 150, 255);
-			}
-			if (velocity < 0) {
-				inputr = map(abs(velocity), 0, 100, 150, 255);
-			}
-
-			analogWrite(controll, inputl);	
-			analogWrite(controlr, inputr);
+		if (velocity < 0) {
+			inputr = map(abs(velocity), 0, 100, 0, 255);
 		}
 
-		int read() {
-			return velocity;
-		}
+		analogWrite(controll, inputl);
+		analogWrite(controlr, inputr);
+	}
+
+	int read() {
+		return velocity;
+	}
 };
 
 // ---------- Actuators Declaration
@@ -182,188 +181,111 @@ Motor motor[2] = {Motor(5, 6), Motor(10, 11)}; // Left, Right
 
 Obstacle    obstacle      = Obstacle(8);
 Inclination inclination   = Inclination(9);
-Infrared    infrared[2]   = {Infrared(A0), Infrared(A1)};             // Left,    Right
-Ultrasonic  ultrasonic[2] = {Ultrasonic(A2, A3), Ultrasonic(A4, A5)}; // Frontal, Lateral
-
-// ---------- Variables
-
-int range[2] = {30, 80};
+Infrared    infrared[2]   = {Infrared(A4), Infrared(A5)};             // Left,    Right
+Ultrasonic  ultrasonic[2] = {Ultrasonic(A0, A1), Ultrasonic(A2, A3)}; // Frontal, Lateral
 
 // ---------- Functions
-
-int print(int &message) {
-	Serial.begin(9600);
+/*
+int print(int message) {
 	Serial.print(message);
-	Serial.end();
 }
 
-int println(int &message) {
-	Serial.begin(9600);
+int println(int message) {
 	Serial.println(message);
-	Serial.end();
 }
 
-int print(String &message) {
-	Serial.begin(9600);
+int print(String message) {
 	Serial.print(message);
-	Serial.end();
 }
 
-int println(String &message) {
-	Serial.begin(9600);
+int println(String message) {
 	Serial.println(message);
-	Serial.end();
 }
-
-void move(int velocity = 100) {
-	motor[0].write(velocity);
-	motor[1].write(velocity);
-}
+*/
 
 void move(int left, int right) {
 	motor[0].write(left);
 	motor[1].write(right);
 }
 
-void advance(int time, int velocity = 100) {
-	move(velocity);
-
-	delay(time);	
-}
-
-void rotate(int angle, int velocity = 100) {
-	int value = 1000; // Need Calibrate
-
-	int time = map(angle, 0, 360, 0, value);
-
-	if (angle < 0) {
-		move(-velocity, velocity);
-	}
-	else {
-		move(velocity, -velocity);
-	}
-	
-	delay(time);
-
-	move(0);
-}
-
-int normalize(int value) {
-	if (value < range[0]) {return 1;} // if black
-	if (value < range[1]) {return 2;} // if signal
-
-	return 0; // if white
-}
-
 int error() {
-	int left  = normalize(infrared[0].read());
-	int right = normalize(infrared[1].read());
+	int left  = infrared[0].read();
+	int right = infrared[1].read();
 
-	if (left == right) {
-		if (left == 2) {return 3;}
-		if (left != 2) {return 0;}
-	}
-	else {
-		if (left  == 2) {return -2;}
-		if (right == 2) {return  2;}
-
-		if (left  == 1) {return -1;}
-		if (right == 1) {return  1;}
-	}
-
-	return 0;
+	return right - left;
 }
 
 void curve() {
-	int deviation = error();
+	int detour = error();
 
-	if (deviation != 0) {
-		int left  = motor[0].read();
-		int right = motor[1].read();
+	int time;
+	int velocity;
 
-		int angle = signal(deviation);
+	if (detour) {
+		time = 50;
+		velocity = 100;
 
-		int velocity = 100;
-
-		switch (module(deviation)) {
-			case 1:
-				angle *= 5;
-				break;
-			case 2:
-				angle *= 60;
-				break;
-			case 3:
-				angle = 180;
-				break;
-		}
-
-		rotate(angle, velocity)
+		int left  = detour > 0 ? velocity : -velocity;
+		int right = detour < 0 ? -velocity : velocity;
 
 		move(left, right);
+
+		int step = 0;
+
+		while (true) {
+			delay(time);
+			
+			if (!error()) {
+				for (step = 0; step < 100; step++) {
+					if (!error()) {
+						
+					}
+				}
+				break;
+			}
+		}
 	}
+	else {
+		time = 50;
+		velocity = 50;
+
+		move(velocity, velocity);
+
+		delay(time);
+		move(0, 0);
+		delay(time);
+	}
+
+	/*
+	switch (detour) {
+		case 0:
+			move(50, 50);
+			break;
+		case -1:
+			move(0, 100);
+			break;
+		case 1:
+			move(100, 0);
+			break;
+	}
+	*/
 }
 
-// --------------- Incomplete
 void dodge() {
-	int distance = obstacle.read();
-
-	int radius = 10;
-
-	if (distance < radius) {
-		int left  = motor[0].read();
-		int right = motor[1].read();
-
-		int _left  = 10;
-		int _right = ((radius + 15) / radius) * _left;
-
-		move(left, right);
-	}
-}
-
-bool arrived() {
-	if (inclination.read()) {
-		if (ultrasonic[1].read() < 10) {
-			return true;
-		}
-	}
-
-	return false;
-}
-
-void end() {
-	ultrasonic[0].disable();
-	ultrasonic[1].disable();
-
-	servo[0].disable();
-	servo[1].disable();
-
-	motor[0].disable();
-	motor[1].disable();
-
-	while (true);
+	if ()
 }
 
 void route() {
 	while (true) {
-		
-
-		if (arrived()) {
-			break;
-		}
+		curve();
+		dodge();
 	}
 }
 
-void rescue() {
-	while (true) {
-
-	}
-
-	end();
+void setup() {
+	delay(5000);
 }
-
-void setup() {}
 
 void loop() {
 	route();
-	rescue();
 }
